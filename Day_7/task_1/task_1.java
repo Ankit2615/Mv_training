41217ac04c1cec00b381eacfcba70801552f8fb4
Ankit Rana <107254369+Ankit2615@users.noreply.github.com>
2023-08-15 11:29:31 +0530

    Delete Day_7/task_1 directory

--- Day_7/task_1/task_1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*; 
-class task_1{
-    public static void main(String args []){
-        Scanner sc = new Scanner(System.in); 
-        String usrInput = sc.nextLine(); 
-        usrInput = usrInput.replaceAll("\\s","");
-        System.out.print("After removing space :- "+ usrInput);  
-    }
-}
